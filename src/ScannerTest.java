15d2ecdfb08c2dc75cf4cd6e2e2b20bd4557a9e2
Rinoyt <Sasha1Sim5@gmail.com>
2025-02-12 22:56:05 +0300

    Prettified tests

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -8,26 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ScannerTest {
     @Test
     public void intTest() {
-        hasInt("100");
-        validInt(100, "100");
-        hasInt("   -100      \n       34567   ");
-        validInt(-100, "   -100      \n       34567   ");
-        validInt(34567, "      \n       34567   ");
-    }
+        String[] inputs = new String[]{
+                "100",
+                "   -100      \r\n\r       34567   "
+        };
+        
+        int[][] expected = new int[][]{
+                {100},
+                {-100, 34567}
+        };
 
-    private static void hasInt(String input) {
-        try (Scanner in = new Scanner(input)) {
-            assertTrue(in.hasNextInt());
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
-    }
-
-    private static void validInt(int expect, String input) {
-        try (Scanner in = new Scanner(input)) {
-            assertEquals(expect, in.nextInt());
-        } catch (IOException e) {
-            throw new AssertionError(e);
+        for (int i = 0; i < inputs.length; i++) {
+            try (Scanner in = new Scanner(inputs[i])) {
+                for (int expect : expected[i]) {
+                    assertTrue(in.hasNextInt());
+                    assertEquals(expect, in.nextInt());
+                }
+            } catch (IOException e) {
+                throw new AssertionError(e);
+            }
         }
     }
 }
